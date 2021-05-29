dd25608722baa8e63ee021d8aaadece7a1d2752d
Berkin Yıldıran <berkin.yildiran@yahoo.com>
2021-05-29 19:58:39 +0300

    fixed login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,6 @@ import { LoginService } from "../services/login.service";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   email: string;
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,35 +11,6 @@ module.exports = {
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
-      spacing: {
-        'login': '450px',
-      },
-      opacity: {
-        '1': '0.01',
-        '2': '0.02',
-        '3': '0.03',
-        '4': '0.04',
-      },
-      colors: {
-        primary: ({ opacityVariable, opacityValue }) => {
-          if (opacityValue !== undefined) {
-            return `rgba(var(--color-primary), ${opacityValue})`
-          }
-          if (opacityVariable !== undefined) {
-            return `rgba(var(--color-primary), var(${opacityVariable}, 1))`
-          }
-          return `rgb(var(--color-primary))`
-        },
-        secondary: ({ opacityVariable, opacityValue }) => {
-          if (opacityValue !== undefined) {
-            return `rgba(var(--color-secondary), ${opacityValue})`
-          }
-          if (opacityVariable !== undefined) {
-            return `rgba(var(--color-secondary), var(${opacityVariable}, 1))`
-          }
-          return `rgb(var(--color-secondary))`
-        },
-      },
       fontFamily: {
         poppins: ["Poppins"],
       },
